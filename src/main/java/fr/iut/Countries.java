cd2ae99a0a7aa5aa0ba916c7dbfc12d5bacb82d5
Ugo Bronszewski <ugo.bronszewski@gmail.com>
2019-10-09 15:16:30 +0200

    [TEST] IslandTaxes OK

--- src/main/java/fr/iut/Countries.java
+++ src/main/java/fr/iut/Countries.java
@@ -2,7 +2,12 @@ package fr.iut;
 
 public enum Countries {
 
-    France(1.2);
+    France(1.2),
+    Island(1.255),
+    Suisse(1.08),
+    Croatie(1.25),
+    Danemark(1.25),
+    Allemagne(1.19);
 
     private final Double taxToApply;
 
--- src/test/java/fr/iut/TestShoppingCart.java
+++ src/test/java/fr/iut/TestShoppingCart.java
@@ -39,4 +39,17 @@ public class TestShoppingCart {
 
         Assert.assertEquals(shoppingCart.getPriceWithTaxes(Countries.France), 51, 0.0001);
     }
+
+    @Test
+    public void IslandTaxes() {
+        final ShoppingCart shoppingCart = new ShoppingCart();
+        final Item carrot = new Item("Carrot", 2.5);
+        shoppingCart.addItem(carrot, 1);
+        shoppingCart.addItem(new Item("Apple", 1), 25);
+        shoppingCart.addItem(new Item("Bananas 1kg", 5), 3);
+
+        Assert.assertEquals(shoppingCart.getPriceWithTaxes(Countries.Island), 42.5 * Countries.Island.getTaxToApply(), 0.0001);
+    }
+
+
 }
