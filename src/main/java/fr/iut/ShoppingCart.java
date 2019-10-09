f5df09bb509341b7ce560ad9fc55fbcf33924b70
Ugo Bronszewski <ugo.bronszewski@gmail.com>
2019-10-09 14:03:25 +0200

    First successful test

---
+++ src/main/java/fr/iut/ShoppingCart.java
@@ -0,0 +1,4 @@
+package fr.iut;
+
+public class ShoppingCart {
+}
---
+++ src/test/java/fr/iut/TestShoppingCart.java
@@ -0,0 +1,11 @@
+package fr.iut;
+
+import org.junit.Test;
+
+public class TestShoppingCart {
+
+    @Test
+    public void addItem() {
+        final ShoppingCart shoppingCart = new ShoppingCart();
+    }
+}
